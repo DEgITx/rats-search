072a40d3e1cd001a3980d31635f6e1a5d6be5cb4
Alexey Kasyanchuk <degitx@gmail.com>
2017-01-15 16:52:06 +0300

    исправление обращения по https

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import './router';
 import PagesPie from './pages-pie.js';
 
 var io = require("socket.io-client");
-window.torrentSocket = io('http://' + document.location.hostname + (process.env.NODE_ENV === 'production' ? '/' : ':8095/'));
+window.torrentSocket = io(document.location.protocol + '//' + document.location.hostname + (process.env.NODE_ENV === 'production' ? '/' : ':8095/'));
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
  
