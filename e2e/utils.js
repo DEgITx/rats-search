9829b84e824cc69aa8543cabed002a7923e76db4
Alexey Kasyanchuk <degitx@gmail.com>
2018-01-31 20:34:50 +0300

    chore(tests): tests simplified

--- e2e/utils.js
+++ e2e/utils.js
@@ -2,18 +2,18 @@ import electron from "electron";
 import { Application } from "spectron";
 
 const beforeEach = function() {
-  this.timeout(10000);
+  this.timeout(15000);
   this.app = new Application({
     path: electron,
     args: ["."],
-    startTimeout: 10000,
-    waitTimeout: 10000
+    startTimeout: 15000,
+    waitTimeout: 15000
   });
   return this.app.start();
 };
 
 const afterEach = function() {
-  this.timeout(10000);
+  this.timeout(15000);
   if (this.app && this.app.isRunning()) {
     return this.app.stop();
   }
