4837293af4a1e68a697ad281c9d2d2590a4fc8c6
Alexey Kasyanchuk <degitx@gmail.com>
2018-04-01 02:52:27 +0300

    fix(tests): fix quit timeout

--- tests/utils.js
+++ tests/utils.js
@@ -7,7 +7,8 @@ const beforeEach = function() {
     path: electron,
     args: ["."],
     startTimeout: 30000,
-    waitTimeout: 30000
+    waitTimeout: 30000,
+    quitTimeout: 10000
   });
   return this.app.start();
 };
