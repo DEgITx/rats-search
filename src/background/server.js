a2ade1cd68a4a3b93dc7f6a04773dbc35d3d4bd1
Alexey Kasyanchuk <degitx@gmail.com>
2018-04-05 16:57:32 +0300

    fix(server): restored server messages

--- src/background/server.js
+++ src/background/server.js
@@ -3,12 +3,31 @@ const spiderCall = require('./spider')
 const dbPatcher = require('./dbPatcher')
 const startSphinx = require('./sphinx')
 
+const http = require('http')
+const express = require('express');
+const app = express();
+const server = http.Server(app);
+const io = require('socket.io')(server);
+
+server.listen(appConfig.httpPort);
+console.log('Listening web server on', appConfig.httpPort, 'port')
+
+app.use(express.static('web', {index: false}));
+
+const socketMessages = {}
+
+io.on('connection', (socket) =>
+{
+  for(const message in socketMessages)
+  {
+    socket.on(message, socketMessages[message])
+  }
+})
+
 sphinx = startSphinx(() => {
   dbPatcher(() => {
-    spider = spiderCall((...data) => { 
-      
-    }, (message, callback) => {
-
+    spider = spiderCall((...data) => io.sockets.emit(...data), (message, callback) => {
+      socketMessages[message] = callback
     }, './', '0.7.1', 'development')
   }, null, sphinx)
 }, './', () => {})
