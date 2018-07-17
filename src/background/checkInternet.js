5be36fa551346c7f17c643573584c66f86e2abb3
Alexey Kasyanchuk <degitx@gmail.com>
2018-07-17 12:18:30 +0300

    fix(network): fix network availability detection #45

--- src/background/checkInternet.js
+++ src/background/checkInternet.js
@@ -1,7 +1,7 @@
 // https://stackoverflow.com/questions/15270902/check-for-internet-connectivity-in-nodejs
 module.exports = function checkInternet(cb) {
 	require('dns').lookup('google.com',function(err) {
-		if (err && err.code == "ENOTFOUND") {
+		if (err && (err.code == "ENOTFOUND" || err.code == "EAI_AGAIN")) {
 			cb(false);
 		} else {
 			cb(true);
