cfb8235f2bf2e783b08f4d678c2a117a25edafd6
Alexey Kasyanchuk <degitx@gmail.com>
2018-07-30 17:46:46 +0300

    feat(nyaa): support of nyaa torrents

---
+++ src/background/strategies/nyaa.js
@@ -0,0 +1,30 @@
+
+const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+
+module.exports = class Nyaa
+{
+	async findHash(hash)
+	{
+		this.hash = hash
+		return await this.parse()
+	}
+
+	async parse()
+	{
+		let html = await fetch('https://nyaa.si/' + (this.threadId ? `view/${this.threadId}` : (this.hash ? `?q=${this.hash}` : '')))
+		if(!html)
+			return
+		html = await html.text()
+		const $ = cheerio.load(html)
+		let topicTitle = $('.panel-title').first().text()
+		if(!topicTitle)
+			return
+
+		topicTitle = topicTitle.replace(/\t/g, '').replace(/\n/g, '')
+		return {
+			name: topicTitle,
+			description: $('#torrent-description').text(),
+		}
+	}
+}
