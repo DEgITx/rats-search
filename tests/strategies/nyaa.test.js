87abd7c00eaa4ec524f7992844e68f4cb880a3c6
Alexey Kasyanchuk <degitx@gmail.com>
2018-08-16 07:32:49 +0300

    nyaa timeout fix

--- tests/strategies/nyaa.test.js
+++ tests/strategies/nyaa.test.js
@@ -5,11 +5,13 @@ describe("nyaa", () => {
 	let nyaa = new Nyaa();
 
 	it("findHash", async function() {
+		this.timeout(10000)
 		const data = await nyaa.findHash('b57cc972ecc31dd31e9349017e5d26c164e08906')
 		assert.equal(data.name, '[HorribleSubs] One Piece - 794 [1080p].mkv')
 	})
 
 	it("notFound", async function() {
+		this.timeout(10000)
 		assert(!await nyaa.findHash('FFEDA8DF683F34A08BE89026114E6C05F881DCC0'))
 	})
 });
