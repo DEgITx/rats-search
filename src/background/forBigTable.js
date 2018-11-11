92d0d13f6d06a552204214c459073a209cf1a37e
Alexey Kasyanchuk <degitx@gmail.com>
2018-11-11 23:44:10 +0300

    perf(architecture): Big performance improvements over big databases and files highlight in search. (#63)
    Reduction database space over 50%. Change database version to v7.
    
    BREAKING CHANGE: databases v6 and v7 are incompatible and need a lot of time for updating (may be even some days/a lot of hours on very big databases)

--- src/background/forBigTable.js
+++ src/background/forBigTable.js
@@ -1,24 +1,24 @@
-module.exports = (sphinx, table, callback, doneCallback, max = 1000, where = '') => new Promise((done) => {
-	const checker = (index = 0) => {
-		sphinx.query(`SELECT * FROM ${table} WHERE id > ${index} ${where} LIMIT ${max}`, (err, torrents) => {
-			const finish = () => {
-				if(err)
-					logTE('sql', 'big table parse error', err)
-				if(doneCallback)
-					doneCallback(true)
-				done(true)
-			}
+module.exports = (sphinx, table, callback, doneCallback, max = 1000, where = '', intermediateCallback = null) => new Promise((done) => {
+	const checker = async (index = 0) => {
+		const finish = () => {
+			if(doneCallback)
+				doneCallback(true)
+			done(true)
+		}
+		const data = await sphinx.query(`SELECT * FROM ${table} WHERE id > ${index} ${where} LIMIT ${max}`);
+		if(data.length == 0) {
+			finish()
+			return;
+		}
 
-			if(!err && torrents.length > 0)
-				Promise.all(torrents.map(callback)).then(() => {
-					if(torrents.length === max)
-						checker(torrents[torrents.length - 1].id)
-					else
-						finish()
-				})
-			else
-				finish()
-		});
+		await Promise.all(data.map(callback));
+		if(intermediateCallback)
+			await intermediateCallback(data[data.length - 1]);
+
+		if(data.length === max)
+			checker(data[data.length - 1].id)
+		else
+			finish()
 	}
 	checker()
 })
