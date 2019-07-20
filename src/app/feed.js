5ccdbc01fead0d60f428ea01b51dda127a8fb3b7
Alexey Kasyanchuk <degitx@gmail.com>
2019-07-21 02:32:30 +0300

    feat(feed): auto-loading feed list

--- src/app/feed.js
+++ src/app/feed.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
+import AutoScrollable from './auto-scrollable'
 import TorrentLine from './torrent'
 import {List} from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import Subheader from 'material-ui/Subheader';
 
-export default class RecentTorrents extends Component {
+export default class RecentTorrents extends AutoScrollable {
 	constructor() {
 		super()
 		this.torrents = [];
 	}
 	componentDidMount() {
-		window.torrentSocket.emit('feed', window.customLoader((data) => {
-			if(data) {
-				this.torrents = data;
-				this.forceUpdate();
-			}
-		}))
+		super.componentDidMount();
+		this.loadMoreFeed();
 		this.feedFunc = ({feed}) => {
-			this.torrents = feed
+			this.torrents = feed.slice(0, this.torrents.length || 20)
 			this.forceUpdate()
 		};
 		window.torrentSocket.on('feedUpdate', this.feedFunc);
 	}
 	componentWillUnmount() {
+		super.componentWillUnmount();
 		if(this.feedFunc)
 			window.torrentSocket.off('feedUpdate', this.feedFunc);
+	}
+	onBottomScroll() {
+		this.loadMoreFeed();
+	}
+	loadMoreFeed() {
+		window.torrentSocket.emit('feed', this.torrents.length, window.customLoader((data) => {
+			if(data) {
+				this.torrents = this.torrents.concat(data);
+				this.forceUpdate();
+			}
+		}))
 	}
 	render() {
 		return (
